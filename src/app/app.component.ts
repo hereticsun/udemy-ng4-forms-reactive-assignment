c8678f59cb5f870afd1ed5c01c4c45a4f8cb2a9f
Azlan Cuttilan <azlan@bynd.com>
2017-06-21 12:04:59 +0100

    Basic form
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  statuses = ['Stable', 'Critical', 'Finished'];
+  projectForm: FormGroup;
+  forbiddenNames = ['Test'];
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'project': new FormControl(null, [Validators.required]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl
+    });
+  }
+
+  onSubmit() {
+     console.log(this.projectForm);
+  }
 }
