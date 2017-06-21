e9500a567a07707503ac22f2e5036106fc8fa879
Azlan Cuttilan <azlan@bynd.com>
2017-06-21 16:22:27 +0100

    Completed assignment
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,20 +15,35 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      'project': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+      // 'project': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+      'project': new FormControl(null, Validators.required, this.forbiddenNamesAsync),
       'email': new FormControl(null, [Validators.required, Validators.email]),
-      'status': new FormControl
+      'status': new FormControl('Stable')
     });
   }
 
   onSubmit() {
-     console.log(this.projectForm);
+     console.log(this.projectForm.value);
   }
 
-  forbiddenNames(control: FormControl): {[s: string]: boolean} {
-    if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
-      return {'nameIsForbidden': true};
-    }
-    return null;
+  // forbiddenNames(control: FormControl): {[s: string]: boolean} {
+  //   if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+  //     return {'nameIsForbidden': true};
+  //   }
+  //   return null;
+  // }
+
+  forbiddenNamesAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+          console.log(control);
+        if (control.value === 'Test') {
+          resolve({'nameIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
